abcd425b3f5376f99c402b258c6a0bf7f6fb7a82
Andreea <andreea.stoenescu@hotmail.com>
2022-06-11 19:11:03 +0100

    initial routing

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+    res.send('Hello!');
+});
+app.get('/messages', (req, res) => {
+    res.send({
+        id: 1,
+        name: 'Andreea'
+    });
+});
+
+app.post('/messages', (req, res) => {
+    console.log('Updating messages');
+});
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
