3224857352c9fb9ea1f56315982f7ad82a1fc8aa
Andreea <andreea.stoenescu@hotmail.com>
2022-06-12 14:24:34 +0100

    serve a static html page

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 function getMessages(req, res) {
     // res.send('<ul><li>Hello!</li></ul>');
-    res.sendFile(path.join(__dirname, '..', 'public', '5-puppers-in-the-grass.jpg'));
+    res.sendFile(path.join(__dirname, '..', 'public', 'images', '5-puppers-in-the-grass.jpg'));
 }
 
 function postMessage(req, res) {
