ae24959764ec7273c9200c4f0897634c63f0334e
Andreea <andreea.stoenescu@hotmail.com>
2022-06-12 13:53:29 +0100

    serve an image

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,8 @@
+const path = require('path');
+
 function getMessages(req, res) {
-    res.send('<ul><li>Hello!</li></ul>');
+    // res.send('<ul><li>Hello!</li></ul>');
+    res.sendFile(path.join(__dirname, '..', 'public', '5-puppers-in-the-grass.jpg'));
 }
 
 function postMessage(req, res) {
