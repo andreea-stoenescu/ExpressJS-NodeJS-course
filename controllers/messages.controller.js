2f21efd03d7a8acdb2a52698555c38a4843af1db
Andreea <andreea.stoenescu@hotmail.com>
2022-06-12 14:54:21 +0100

    use a common layout for two pages

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -2,7 +2,11 @@ const path = require('path');
 
 function getMessages(req, res) {
     // res.send('<ul><li>Hello!</li></ul>');
-    res.sendFile(path.join(__dirname, '..', 'public', 'images', '5-puppers-in-the-grass.jpg'));
+    // res.sendFile(path.join(__dirname, '..', 'public', 'images', '5-puppers-in-the-grass.jpg'));
+    res.render('messages', {
+        title: 'Messages',
+        friend: 'Andreea',
+    })
 }
 
 function postMessage(req, res) {
